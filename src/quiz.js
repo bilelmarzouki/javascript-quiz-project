dc677d56cefed3248fd739e08992a430c24ef698
Bilel Marzouki <bilelmarzou@gmail.com>
2026-01-19 17:59:23 +0100

    finish the first day coding

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,39 @@
+
 class Quiz {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+        constructor (questions, timeLimit, timeRemaining){
+            this.questions=questions;
+            this.timeLimit=timeLimit;
+            this.timeRemaining=timeRemaining;
+            this.correctAnswers=0;
+            this.currentQuestionIndex=0;
+        }
+       getQuestion(){
+           return this.questions[this.currentQuestionIndex]
+       } 
+       moveToNextQuestion(){
+          return this.currentQuestionIndex+=1
+       }
+       shuffleQuestions(){
+           for (let i =this.questions.length - 1; i > 0; i--) {
+               const j = Math.floor(Math.random() * (i + 1));
+              [this.questions[i],this.questions[j]] = [this.questions[j],this.questions[i]];
+           }
+           return this.questions;
+       }
+       checkAnswer(answer){
+         if(answer===this.getQuestion().answer){
+            this.correctAnswers+=1
+         }
+       }
 
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+       hasEnded(){
+          if(this.currentQuestionIndex<this.questions.length){
+            return false
+          }else{
+            return true
+          }
+       }
 }
+
